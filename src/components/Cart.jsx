781a84a2a240c5a1bb6f208d8f41ef0682f67415
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2023-07-19 22:42:41 -0500

    correccion de storage

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,106 +1,123 @@
-import './Cart.css'
+import './Cart.css';
 
-import {useEffect, useId} from 'react'
-import {ClearCartIcon} from './Icons.js'
-import {addToCart, clearCart, restToCart} from "../redux/cartSlice";
-import {useDispatch, useSelector} from "react-redux";
-import {Wompi} from "./Wompi.js";
+import { useEffect, useId, useState } from 'react';
+import { ClearCartIcon } from './Icons.js';
+import { addToCart, clearCart, restToCart, updateCart } from '../redux/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { Wompi } from './Wompi.js';
 
-function CartItem({thumbnail, price, title, quantity, totalPrice, addToCart, restToCart}) {
+function CartItem({ thumbnail, price, title, quantity, totalPrice, addToCart, restToCart }) {
+  return (
+    <li>
+      <small className="circle-background" data-testid="quantityVAlue">
+        {quantity}
+      </small>
+      <img src={thumbnail} alt={title} />
+      <footer>
+        <label>
+          <small>{title}:</small> ${totalPrice}
+        </label>
 
-    return (
-        <li>
-            <small className="circle-background" data-testid="quantityVAlue">
-                {quantity}
-            </small>
-            <img
-                src={thumbnail}
-                alt={title}
-            />
-            <footer>
-                <label>
-                    <small>{title}:</small> ${totalPrice}
-                </label>
-
-                <label>
-                    <button onClick={(product) => restToCart()}>-
-                    </button>
-                    <button onClick={(product) => addToCart()}>+
-                    </button>
-                </label>
-            </footer>
-        </li>
-    )
+        <label>
+          <button onClick={(product) => restToCart()}>-</button>
+          <button onClick={(product) => addToCart()}>+</button>
+        </label>
+      </footer>
+    </li>
+  );
 }
 
 export function Cart() {
-    const cartCheckboxId = useId()
-    const dispatch = useDispatch();
-    let carts = useSelector((state) => state.cart.cart);
-    const showCart = useSelector((state) => state.cart.showCart);
+  const cartCheckboxId = useId();
+  const dispatch = useDispatch();
+  let carts = useSelector((state) => state.cart.cart);
+  const showCart = useSelector((state) => state.cart.showCart);
+  const [first, setFirsts] = useState(0);
+  const [parsedCarts, setparsedCartss] = useState([]);
 
-    useEffect(() => {
-        const savedCarts = localStorage.getItem('cart', JSON.stringify(carts));
-        if (savedCarts && savedCarts.length > 0) {
-            const parsedCarts = JSON.parse(savedCarts)
-            parsedCarts.map((e) => {
-                dispatch(addToCart(e))
-            })
-        }
-    }, []);
+  useEffect(() => {
+    console.log('parsedCarts', parsedCarts);
+    const savedCarts = localStorage.getItem('cart', JSON.stringify(carts));
+    if (savedCarts && savedCarts.length > 0) {
+      const parsedCarts = JSON.parse(savedCarts);
+      setparsedCartss(parsedCarts);
+      console.log('parsedCartsenelif', parsedCarts);
 
-    useEffect(() => {
-        localStorage.setItem('cart', JSON.stringify(carts));
-    }, [carts]);
+      dispatch(updateCart(parsedCarts));
 
-    const handlerClearCart = () => {
-        dispatch(clearCart());
-    };
+      // parsedCarts.map((e) => {
+      //   dispatch(addToCart(e));
+      // });
+    }
+    setFirsts(1);
+  }, []);
 
-    const handleAddToCart = (product) => {
-        dispatch(addToCart(product))
-        localStorage.setItem('cart', JSON.stringify(carts));
+  useEffect(() => {
+    console.log('parsedCartsCrtscambia', parsedCarts);
+    console.log('cartsenuseffectcarts', carts);
+    if (first != 0) {
+      console.log('firstttttttttt', carts);
+      setparsedCartss(carts);
+      localStorage.setItem('cart', JSON.stringify(carts));
+    }
+  }, [carts]);
 
-    };
-    const handleRestToCart = (product) => {
-        dispatch(restToCart(product));
-    };
+  const handlerClearCart = () => {
+    dispatch(clearCart());
+  };
 
-    return (
-        <>
-            {showCart ? <aside className='cart'>
-                <ul>
-                    <h4>
-                        Shopping Cart
-                    </h4>
-                </ul>
-                {<ul>
-                    {carts.map(product => (
-                        <div>
-                            <CartItem
-                                key={product.id}
+  const handleAddToCart = (product) => {
+    console.log('aaaaaaaaaaaa', product);
+    dispatch(addToCart(product));
 
-                                addToCart={() => {
-                                    handleAddToCart(product)
-                                }}
-                                restToCart={() => {
-                                    handleRestToCart(product)
-                                }}
-                                {...product}
-                            /></div>
-                    ))}
-                </ul>
-                }
-                <button onClick={() => handlerClearCart()}>
-                    <ClearCartIcon/>
-                </button>
-            </aside> : "Agrega Productos a tu carrito!"}
-            {/*pendiente wompi porque no pude obtener la key*/}
-            <button onClick={() => handlerClearCart()}>
-                Pagar con wompi
-                <Wompi/>
-            </button>
-            <Wompi/>
-        </>
-    )
+    localStorage.setItem('cart', JSON.stringify(carts));
+  };
+  const handleRestToCart = (product) => {
+    dispatch(restToCart(product));
+    localStorage.setItem('cart', JSON.stringify(carts));
+  };
+
+  return (
+    <>
+      {showCart ? (
+        <aside className="cart">
+          <ul>
+            <h4>
+              Shopping Cart
+              {/* {JSON.stringify(parsedCarts,null,2)} */}
+            </h4>
+          </ul>
+          {
+            <ul>
+              {parsedCarts.map((product) => (
+                <div>
+                  <CartItem
+                    key={product.id}
+                    addToCart={() => {
+                      handleAddToCart(product);
+                    }}
+                    restToCart={() => {
+                      handleRestToCart(product);
+                    }}
+                    {...product}
+                  />
+                </div>
+              ))}
+            </ul>
+          }
+          <button onClick={() => handlerClearCart()}>
+            <ClearCartIcon />
+          </button>
+        </aside>
+      ) : (
+        'Agrega Productos a tu carrito!'
+      )}
+      {/*pendiente wompi porque no pude obtener la key*/}
+      <button onClick={() => handlerClearCart()}>
+        Pagar con wompi
+        <Wompi />
+      </button>
+      <Wompi />
+    </>
+  );
 }
--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,71 +1,76 @@
-import {createSlice, current} from '@reduxjs/toolkit';
+import { createSlice, current } from '@reduxjs/toolkit';
 
 const initialState = {
-    products: [],
-    cart: [],
-    cartStore: [],
-    showCart: false,
+  products: [],
+  cart: [],
+  cartStore: [],
+  showCart: false,
 };
 
 export const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        addToCart: (state, action) => {
-            const productIncart = state.cart.findIndex(item => current(item).id == action.payload.id)
-            if (productIncart >= 0) {
-                let priceProduct = action.payload.price
-                state.cart[productIncart].quantity += 1;
-                state.cart[productIncart].totalPrice += priceProduct;
-            } else {
-                let priceProduct = action.payload.price
-                let newCart = action.payload;
-                newCart = Object.assign({}, action.payload);
-                newCart.quantity = 1;
-                newCart.totalPrice = priceProduct;
-                state.cart.push(newCart)
-            }
-        },
-        addToCartFromLocal: (state, action) => {
-            const savedCarts =   localStorage.getItem('cart');
-            state.cartStore=  JSON.parse(savedCarts)
-        },
-        restToCart: (state, action) => {
-            const productIncart = state.cart.findIndex(item => current(item).id === action.payload.id)
-            if (productIncart >= 0 && state.cart[productIncart].quantity > 0) {
-                let priceProduct = action.payload.price
-                state.cart[productIncart].quantity -= 1;
-                state.cart[productIncart].totalPrice -= priceProduct;
-            }
-        },
-        removeFromCart: (state, action) => {
-            const cartIdToRemove = action.payload;
-            state.cart = state.cart.filter(
-                (cart) => cart.id !== cartIdToRemove.id
-            );
-        },
-        clearCart: (state, action) => {
-            const cartIdToRemove = [];
-            state.cart = [];
-        },
-        addProduct: (state, action) => {
-            const newProduct = action.payload;
-            state.products.push(newProduct);
-        },
-        ShowCart: (state, action) => {
-            state.showCart = action.payload;
-        },
+  name: 'cart',
+  initialState,
+  reducers: {
+    updateCart: (state, action) => {
+      state.cart = action.payload;
     },
+    addToCart: (state, action) => {
+      const productIncart = state.cart.findIndex((item) => current(item).id == action.payload.id);
+      console.log('productIncart', productIncart);
+      if (productIncart >= 0) {
+        let priceProduct = action.payload.price;
+        console.log('state.cart[productIncart].quantity', current(state.cart[productIncart]));
+        state.cart[productIncart].quantity += 1;
+        state.cart[productIncart].totalPrice += priceProduct;
+      } else {
+        let priceProduct = action.payload.price;
+        let newCart = action.payload;
+        newCart = Object.assign({}, action.payload);
+        newCart.quantity = 1;
+        newCart.totalPrice = priceProduct;
+        state.cart.push(newCart);
+      }
+    },
+    addToCartFromLocal: (state, action) => {
+      const savedCarts = localStorage.getItem('cart');
+      state.cartStore = JSON.parse(savedCarts);
+    },
+    restToCart: (state, action) => {
+      console.log('restando');
+      const productIncart = state.cart.findIndex((item) => current(item).id === action.payload.id);
+      if (productIncart >= 0 && state.cart[productIncart].quantity > 0) {
+        let priceProduct = action.payload.price;
+        state.cart[productIncart].quantity -= 1;
+        state.cart[productIncart].totalPrice -= priceProduct;
+      }
+    },
+    removeFromCart: (state, action) => {
+      const cartIdToRemove = action.payload;
+      state.cart = state.cart.filter((cart) => cart.id !== cartIdToRemove.id);
+    },
+    clearCart: (state, action) => {
+      const cartIdToRemove = [];
+      state.cart = [];
+    },
+    addProduct: (state, action) => {
+      const newProduct = action.payload;
+      state.products.push(newProduct);
+    },
+    ShowCart: (state, action) => {
+      state.showCart = action.payload;
+    },
+  },
 });
 
 export const {
-    addToCart,
-    addToCartFromLocal,
-    restToCart,
-    removeFromCart,
-    addProduct,
-    clearCart,
-    ShowCart
+  addToCart,
+  updateCart,
+  addToCartFromLocal,
+  restToCart,
+  removeFromCart,
+  addProduct,
+  clearCart,
+  ShowCart,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
